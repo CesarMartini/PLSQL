f6d3796923b3c992bff8bb91407a1fc197441cb7
Lucas Oliveira <loslucassilva@gmail.com>
2013-10-21 19:48:26 -0200

    Create procedure-p_print.sql
    My first procedure to github.

---
+++ procedure-p_print.sql
@@ -0,0 +1,14 @@
+declare
+  v_tx VARCHAR2(100) := 'I just printed my <in> line!';
+    
+    procedure p_print(i_string_tx in VARCHAR2, i_replace_tx in VARCHAR2 := 'new')
+    is
+    begin
+      dbms_output.put_line(replace(i_string_tx, '<in>', i_replace_tx));
+    end;
+
+  begin
+    p_print(v_tx, 'fisrt');
+    p_print(v_tx, 'second');
+    p_print(v_tx);
+end;
